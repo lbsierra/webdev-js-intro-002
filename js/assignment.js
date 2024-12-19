dd1929f113f99b7d43ebf8fe9630882541633b43
Sierra <sonicaradia@gmail.com>
2024-12-19 15:08:58 -0500

    Created willMeet variable, finished functions, added willMeet function

--- js/assignment.js
+++ js/assignment.js
@@ -15,20 +15,26 @@ let willMeetResolution = true;
 
 function updateYear() {
     // write the logic
-
+    yearElement.innerText = currentYear;
 }
 
 function updateResolution() {
     // Update this function 
+    resolutionElement.innerText = resolution;
+}
 
+function updateWillMeetResolution() {
+    willMeetResolutionElement.innerText = willMeetResolution;
 }
 
 function render() {
     // Finish writing this function
-
+    updateResolution();
+    updateYear();
+    updateWillMeetResolution();
 }
 
 submissionBtn.addEventListener("click", function () {
     // Finish writing this function
-
+    render();
 })
