15736f8719a36da347069361e249752ab47f31bf
Sierra <sonicaradia@gmail.com>
2024-12-19 14:37:02 -0500

    Completed part 1 experiment

--- js/index.js
+++ js/index.js
@@ -2,3 +2,42 @@
 // that are not declared
 "use strict"
 
+/*
+const myName = "Sierra";
+console.log(myName);
+console.log(typeof myName);
+
+const moonsOfNirn = 2;
+console.log(moonsOfNirn);
+console.log(typeof moonsOfNirn);
+
+let var0 = null;
+console.log(var0);
+console.log(typeof var0);
+
+let fred 
+console.log(fred);
+console.log(typeof fred);
+
+let isDecember = true;
+console.log(isDecember);
+console.log(typeof isDecember);
+
+
+let intimidatingNumber = BigInt(80000000008);
+console.log(intimidatingNumber);
+console.log(typeof intimidatingNumber);
+
+const symbolVar = Symbol();
+console.log(symbolVar);
+console.log(typeof symbolVar);
+
+let dogBio = {
+    name: "Kylo",
+    age: 7,
+    breed: "Shiba Inu"
+}
+
+console.log(dogBio);
+console.log(typeof dogBio);
+*/
